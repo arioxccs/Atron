e7badeb786cb2a73a6738304e5c3618c40a916f8
Gabriela Chagas <gabrielaapchagas@icloud.com>
2025-10-03 09:19:02 -0300

    Tutorial label Teste

--- Atron/FrmEstoquePrincipal.Designer.cs
+++ Atron/FrmEstoquePrincipal.Designer.cs
@@ -168,9 +168,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(156, 162);
             label1.Name = "label1";
-            label1.Size = new Size(33, 15);
+            label1.Size = new Size(40, 15);
             label1.TabIndex = 4;
-            label1.Text = "Teste";
+            label1.Text = "Tested";
             // 
             // FrmEstoquePrincipal
             // 
