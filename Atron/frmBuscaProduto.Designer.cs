39ea9f8c89c9539878106f44b49301b9ddcaf539
miguel.cardoso9 <miguel.cardoso9@BIBM10>
2025-10-09 11:55:37 -0300

    tela busca produtos 1

---
+++ Atron/frmBuscaProduto.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Atron
+{
+    partial class frmBuscaProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBuscaProduto));
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(66, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Bebas Neue", 26F);
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(122, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(975, 53);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "     Buscar Produto";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(971, 513);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(293, 158);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // frmBuscaProduto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 661);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Name = "frmBuscaProduto";
+            Text = "frmBuscaProduto";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+    }
+}
