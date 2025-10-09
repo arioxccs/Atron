1447594d244a8766d14b3c54635477a64bf9b095
mariaclararicha123@gmail.com <mariaclararicha123@gmail.com>
2025-10-09 00:29:24 -0300

    Criacao da Tela de Dados da Solicitação de Pedidos 1

---
+++ Atron/FrmDadosdaSolicitacoesdePedidos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atron
+{
+    public partial class FrmDadosdaSolicitacoesdePedidos : Form
+    {
+        public FrmDadosdaSolicitacoesdePedidos()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmDadosdaSolicitacoesdePedidos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMeia_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
