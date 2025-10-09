34b23fea94d5b11c02408555f92ce2f6e822bfe4
mariaclararicha123@gmail.com <mariaclararicha123@gmail.com>
2025-10-09 00:50:37 -0300

    Criacao da tela 2 de Dados de solicitacao de pedidos

---
+++ Atron/FrmDadosdaSolicitacaodePedidos2.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Atron
+{
+    partial class FrmDadosdaSolicitacaodePedidos2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDadosdaSolicitacaodePedidos2));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            lblDadosdaSolicitacaodePedidos = new Label();
+            pnliNHA = new Panel();
+            lblMeiaCanoAlto = new Label();
+            lblNumPedido = new Label();
+            lblProduto = new Label();
+            lblCodProduto = new Label();
+            lblMarca = new Label();
+            lblFornecedor = new Label();
+            lblCor = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label15);
+            panel1.Controls.Add(label14);
+            panel1.Controls.Add(label13);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(lblCor);
+            panel1.Controls.Add(lblFornecedor);
+            panel1.Controls.Add(lblMarca);
+            panel1.Controls.Add(lblCodProduto);
+            panel1.Controls.Add(lblProduto);
+            panel1.Controls.Add(lblNumPedido);
+            panel1.Controls.Add(lblMeiaCanoAlto);
+            panel1.Location = new Point(71, 116);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(598, 307);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(1000, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 100);
+            panel2.TabIndex = 1;
+            // 
+            // lblDadosdaSolicitacaodePedidos
+            // 
+            lblDadosdaSolicitacaodePedidos.AutoSize = true;
+            lblDadosdaSolicitacaodePedidos.Font = new Font("Lato Black", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDadosdaSolicitacaodePedidos.ForeColor = Color.FromArgb(42, 97, 141);
+            lblDadosdaSolicitacaodePedidos.Location = new Point(217, 22);
+            lblDadosdaSolicitacaodePedidos.Name = "lblDadosdaSolicitacaodePedidos";
+            lblDadosdaSolicitacaodePedidos.Size = new Size(347, 23);
+            lblDadosdaSolicitacaodePedidos.TabIndex = 2;
+            lblDadosdaSolicitacaodePedidos.Text = "DADOS DA SOLICITAÇÃO DE PEDIDOS";
+            // 
+            // pnliNHA
+            // 
+            pnliNHA.Location = new Point(94, 58);
+            pnliNHA.Name = "pnliNHA";
+            pnliNHA.Size = new Size(539, 2);
+            pnliNHA.TabIndex = 1;
+            // 
+            // lblMeiaCanoAlto
+            // 
+            lblMeiaCanoAlto.AutoSize = true;
+            lblMeiaCanoAlto.Font = new Font("Segoe UI", 14.25F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            lblMeiaCanoAlto.ForeColor = Color.FromArgb(255, 122, 0);
+            lblMeiaCanoAlto.Location = new Point(23, 16);
+            lblMeiaCanoAlto.Name = "lblMeiaCanoAlto";
+            lblMeiaCanoAlto.Size = new Size(142, 25);
+            lblMeiaCanoAlto.TabIndex = 0;
+            lblMeiaCanoAlto.Text = "Meia Cano Alto";
+            // 
+            // lblNumPedido
+            // 
+            lblNumPedido.AutoSize = true;
+            lblNumPedido.ForeColor = Color.FromArgb(99, 131, 157);
+            lblNumPedido.Location = new Point(64, 58);
+            lblNumPedido.Name = "lblNumPedido";
+            lblNumPedido.Size = new Size(126, 15);
+            lblNumPedido.TabIndex = 1;
+            lblNumPedido.Text = "Número do Pedido: 12";
+            // 
+            // lblProduto
+            // 
+            lblProduto.AutoSize = true;
+            lblProduto.ForeColor = Color.FromArgb(99, 131, 157);
+            lblProduto.Location = new Point(64, 84);
+            lblProduto.Name = "lblProduto";
+            lblProduto.Size = new Size(193, 15);
+            lblProduto.TabIndex = 2;
+            lblProduto.Text = "Produto Solicitado: Meia Cano Alto";
+            // 
+            // lblCodProduto
+            // 
+            lblCodProduto.AutoSize = true;
+            lblCodProduto.ForeColor = Color.FromArgb(99, 131, 157);
+            lblCodProduto.Location = new Point(64, 111);
+            lblCodProduto.Name = "lblCodProduto";
+            lblCodProduto.Size = new Size(127, 15);
+            lblCodProduto.TabIndex = 3;
+            lblCodProduto.Text = "Código do Produto: 54";
+            // 
+            // lblMarca
+            // 
+            lblMarca.AutoSize = true;
+            lblMarca.ForeColor = Color.FromArgb(99, 131, 157);
+            lblMarca.Location = new Point(64, 136);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(77, 15);
+            lblMarca.TabIndex = 4;
+            lblMarca.Text = "Marca: Puma";
+            // 
+            // lblFornecedor
+            // 
+            lblFornecedor.AutoSize = true;
+            lblFornecedor.ForeColor = Color.FromArgb(99, 131, 157);
+            lblFornecedor.Location = new Point(64, 164);
+            lblFornecedor.Name = "lblFornecedor";
+            lblFornecedor.Size = new Size(179, 15);
+            lblFornecedor.TabIndex = 5;
+            lblFornecedor.Text = "Fornecedor: Atacado A Esportiva";
+            // 
+            // lblCor
+            // 
+            lblCor.AutoSize = true;
+            lblCor.ForeColor = Color.FromArgb(99, 131, 157);
+            lblCor.Location = new Point(64, 185);
+            lblCor.Name = "lblCor";
+            lblCor.Size = new Size(60, 15);
+            lblCor.TabIndex = 6;
+            lblCor.Text = "Cor: Preto";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = Color.FromArgb(99, 131, 157);
+            label7.Location = new Point(64, 212);
+            label7.Name = "label7";
+            label7.Size = new Size(339, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Observações: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = Color.FromArgb(99, 131, 157);
+            label8.Location = new Point(278, 58);
+            label8.Name = "label8";
+            label8.Size = new Size(125, 30);
+            label8.TabIndex = 8;
+            label8.Text = "Pedido solicitado por: \nMaria Clara";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = Color.FromArgb(99, 131, 157);
+            label9.Location = new Point(278, 111);
+            label9.Name = "label9";
+            label9.Size = new Size(138, 30);
+            label9.TabIndex = 9;
+            label9.Text = "Usuário que confirmou:  \nGabriela Chagas";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = Color.FromArgb(99, 131, 157);
+            label10.Location = new Point(278, 152);
+            label10.Name = "label10";
+            label10.Size = new Size(192, 15);
+            label10.TabIndex = 10;
+            label10.Text = "Quantidade Solicitada: 40 produtos";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = Color.FromArgb(99, 131, 157);
+            label11.Location = new Point(278, 185);
+            label11.Name = "label11";
+            label11.Size = new Size(163, 15);
+            label11.TabIndex = 11;
+            label11.Text = "Quantidade Atual: 3 produtos";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = Color.FromArgb(30, 79, 119);
+            label12.Location = new Point(278, 260);
+            label12.Name = "label12";
+            label12.Size = new Size(113, 15);
+            label12.TabIndex = 12;
+            label12.Text = "Previsão de Entrega:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(278, 275);
+            label13.Name = "label13";
+            label13.Size = new Size(53, 15);
+            label13.TabIndex = 13;
+            label13.Text = "20/03/25";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.ForeColor = Color.FromArgb(30, 79, 119);
+            label14.Location = new Point(147, 260);
+            label14.Name = "label14";
+            label14.Size = new Size(110, 15);
+            label14.TabIndex = 14;
+            label14.Text = "Data da Solicitação:";
+            label14.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(204, 275);
+            label15.Name = "label15";
+            label15.Size = new Size(53, 15);
+            label15.TabIndex = 15;
+            label15.Text = "02/03/25";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(450, 8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(134, 135);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.FromArgb(99, 131, 157);
+            label1.ImageAlign = ContentAlignment.TopRight;
+            label1.Location = new Point(64, 231);
+            label1.Name = "label1";
+            label1.Size = new Size(329, 15);
+            label1.TabIndex = 17;
+            label1.Text = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(568, 272);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 32);
+            button1.TabIndex = 3;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(639, 388);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(42, 41);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 20;
+            pictureBox2.TabStop = false;
+            // 
+            // FrmDadosdaSolicitacaodePedidos2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1124, 562);
+            Controls.Add(pictureBox2);
+            Controls.Add(pnliNHA);
+            Controls.Add(lblDadosdaSolicitacaodePedidos);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "FrmDadosdaSolicitacaodePedidos2";
+            Text = "FrmDadosdaSolicitacaodePedidos2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel pnliNHA;
+        private Label lblDadosdaSolicitacaodePedidos;
+        private Label lblMeiaCanoAlto;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label lblCor;
+        private Label lblFornecedor;
+        private Label lblMarca;
+        private Label lblCodProduto;
+        private Label lblProduto;
+        private Label lblNumPedido;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button1;
+        private PictureBox pictureBox2;
+    }
+}
