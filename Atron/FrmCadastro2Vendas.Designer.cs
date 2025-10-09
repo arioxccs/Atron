f90dfa50e7ca1bf093f2be264d2247ec3ae837e0
JERONIMO <JERONIMO@DESKTOP-02BEGM9>
2025-10-08 22:39:37 -0300

    tela de cadastro de 2 vendas e renomear tela de cadastro de duas encomendas

--- Atron/FrmCadastro2Vendas.Designer.cs
+++ Atron/FrmCadastro2Vendas.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Atron
 {
-    partial class frmCadastro2Encomendas
+    partial class FrmCadastro2Vendas
     {
         /// <summary>
         /// Required designer variable.
@@ -28,15 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Label lblTitulo2;
             Label label1;
-            lblTitulo1 = new Label();
+            Label lblTitulo2;
             pnlFundo = new Panel();
             panel1 = new Panel();
             btnCancelar = new Button();
             btnConfirmar = new Button();
             txtQtdeParcelas2 = new TextBox();
-            txtEntrada2 = new TextBox();
+            txtFormaPgto2 = new TextBox();
             txtValorTotal2 = new TextBox();
             txtQtde2 = new TextBox();
             txtCodigoProduto2 = new TextBox();
@@ -45,52 +44,20 @@
             cmbProduto2 = new ComboBox();
             cmbCliente2 = new ComboBox();
             txtQtdeParcelas = new TextBox();
-            txtEntrada = new TextBox();
+            txtFormaPgto = new TextBox();
             txtValorTotal = new TextBox();
             txtQuantidade = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
+            txtCodigoProduto = new TextBox();
+            txtCodigoCliente = new TextBox();
             cmbData = new ComboBox();
             cmbProduto = new ComboBox();
             cmbCliente = new ComboBox();
-            lblTitulo2 = new Label();
+            lblTitulo1 = new Label();
             label1 = new Label();
+            lblTitulo2 = new Label();
             pnlFundo.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // lblTitulo2
-            // 
-            lblTitulo2.AutoSize = true;
-            lblTitulo2.Font = new Font("Bebas Neue", 30F, FontStyle.Regular, GraphicsUnit.Pixel);
-            lblTitulo2.ForeColor = Color.FromArgb(121, 121, 121);
-            lblTitulo2.Location = new Point(27, 19);
-            lblTitulo2.Name = "lblTitulo2";
-            lblTitulo2.Size = new Size(186, 38);
-            lblTitulo2.TabIndex = 3;
-            lblTitulo2.Text = "BAIXA NO ESTOQUE";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Bebas Neue", 30F, FontStyle.Regular, GraphicsUnit.Pixel);
-            label1.ForeColor = Color.FromArgb(121, 121, 121);
-            label1.Location = new Point(27, 737);
-            label1.Name = "label1";
-            label1.Size = new Size(127, 38);
-            label1.TabIndex = 24;
-            label1.Text = "Cadastro 2";
-            // 
-            // lblTitulo1
-            // 
-            lblTitulo1.AutoSize = true;
-            lblTitulo1.Font = new Font("Bebas Neue", 33F, FontStyle.Regular, GraphicsUnit.Pixel);
-            lblTitulo1.ForeColor = Color.White;
-            lblTitulo1.Location = new Point(35, 25);
-            lblTitulo1.Name = "lblTitulo1";
-            lblTitulo1.Size = new Size(362, 43);
-            lblTitulo1.TabIndex = 1;
-            lblTitulo1.Text = "CADASTRO DE VENDA - ENCOMENDA";
             // 
             // pnlFundo
             // 
@@ -101,7 +68,7 @@
             pnlFundo.MinimumSize = new Size(0, 1663);
             pnlFundo.Name = "pnlFundo";
             pnlFundo.Size = new Size(1270, 1663);
-            pnlFundo.TabIndex = 1;
+            pnlFundo.TabIndex = 2;
             // 
             // panel1
             // 
@@ -110,7 +77,7 @@
             panel1.Controls.Add(btnConfirmar);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(txtQtdeParcelas2);
-            panel1.Controls.Add(txtEntrada2);
+            panel1.Controls.Add(txtFormaPgto2);
             panel1.Controls.Add(txtValorTotal2);
             panel1.Controls.Add(txtQtde2);
             panel1.Controls.Add(txtCodigoProduto2);
@@ -119,11 +86,11 @@
             panel1.Controls.Add(cmbProduto2);
             panel1.Controls.Add(cmbCliente2);
             panel1.Controls.Add(txtQtdeParcelas);
-            panel1.Controls.Add(txtEntrada);
+            panel1.Controls.Add(txtFormaPgto);
             panel1.Controls.Add(txtValorTotal);
             panel1.Controls.Add(txtQuantidade);
-            panel1.Controls.Add(textBox2);
-            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(txtCodigoProduto);
+            panel1.Controls.Add(txtCodigoCliente);
             panel1.Controls.Add(cmbData);
             panel1.Controls.Add(cmbProduto);
             panel1.Controls.Add(cmbCliente);
@@ -145,7 +112,6 @@
             btnCancelar.TabIndex = 26;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = false;
-            btnCancelar.Click += btnCancelar_Click;
             // 
             // btnConfirmar
             // 
@@ -159,6 +125,17 @@
             btnConfirmar.Text = "Confirmar";
             btnConfirmar.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bebas Neue", 30F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label1.ForeColor = Color.FromArgb(121, 121, 121);
+            label1.Location = new Point(27, 737);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 38);
+            label1.TabIndex = 24;
+            label1.Text = "Cadastro 2";
+            // 
             // txtQtdeParcelas2
             // 
             txtQtdeParcelas2.BorderStyle = BorderStyle.FixedSingle;
@@ -170,16 +147,16 @@
             txtQtdeParcelas2.TabIndex = 23;
             txtQtdeParcelas2.Text = "    QTDE PARCELAS";
             // 
-            // txtEntrada2
+            // txtFormaPgto2
             // 
-            txtEntrada2.BorderStyle = BorderStyle.FixedSingle;
-            txtEntrada2.Font = new Font("Bebas Neue", 17F);
-            txtEntrada2.ForeColor = Color.FromArgb(147, 147, 147);
-            txtEntrada2.Location = new Point(392, 1259);
-            txtEntrada2.Name = "txtEntrada2";
-            txtEntrada2.Size = new Size(309, 44);
-            txtEntrada2.TabIndex = 22;
-            txtEntrada2.Text = "    ENTRADA";
+            txtFormaPgto2.BorderStyle = BorderStyle.FixedSingle;
+            txtFormaPgto2.Font = new Font("Bebas Neue", 17F);
+            txtFormaPgto2.ForeColor = Color.FromArgb(147, 147, 147);
+            txtFormaPgto2.Location = new Point(392, 1259);
+            txtFormaPgto2.Name = "txtFormaPgto2";
+            txtFormaPgto2.Size = new Size(309, 44);
+            txtFormaPgto2.TabIndex = 22;
+            txtFormaPgto2.Text = "    forma de pagamento";
             // 
             // txtValorTotal2
             // 
@@ -271,16 +248,16 @@
             txtQtdeParcelas.TabIndex = 14;
             txtQtdeParcelas.Text = "    QTDE PARCELAS";
             // 
-            // txtEntrada
+            // txtFormaPgto
             // 
-            txtEntrada.BorderStyle = BorderStyle.FixedSingle;
-            txtEntrada.Font = new Font("Bebas Neue", 17F);
-            txtEntrada.ForeColor = Color.FromArgb(147, 147, 147);
-            txtEntrada.Location = new Point(427, 580);
-            txtEntrada.Name = "txtEntrada";
-            txtEntrada.Size = new Size(309, 44);
-            txtEntrada.TabIndex = 13;
-            txtEntrada.Text = "    ENTRADA";
+            txtFormaPgto.BorderStyle = BorderStyle.FixedSingle;
+            txtFormaPgto.Font = new Font("Bebas Neue", 17F);
+            txtFormaPgto.ForeColor = Color.FromArgb(147, 147, 147);
+            txtFormaPgto.Location = new Point(427, 580);
+            txtFormaPgto.Name = "txtFormaPgto";
+            txtFormaPgto.Size = new Size(309, 44);
+            txtFormaPgto.TabIndex = 13;
+            txtFormaPgto.Text = "    forma de Pagamento";
             // 
             // txtValorTotal
             // 
@@ -304,27 +281,27 @@
             txtQuantidade.TabIndex = 11;
             txtQuantidade.Text = "    QUANTIDADE";
             // 
-            // textBox2
+            // txtCodigoProduto
             // 
-            textBox2.BorderStyle = BorderStyle.FixedSingle;
-            textBox2.Font = new Font("Bebas Neue", 17F);
-            textBox2.ForeColor = Color.FromArgb(147, 147, 147);
-            textBox2.Location = new Point(808, 291);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(274, 44);
-            textBox2.TabIndex = 10;
-            textBox2.Text = "    CÓDIGO DO PRODUTO";
+            txtCodigoProduto.BorderStyle = BorderStyle.FixedSingle;
+            txtCodigoProduto.Font = new Font("Bebas Neue", 17F);
+            txtCodigoProduto.ForeColor = Color.FromArgb(147, 147, 147);
+            txtCodigoProduto.Location = new Point(808, 291);
+            txtCodigoProduto.Name = "txtCodigoProduto";
+            txtCodigoProduto.Size = new Size(274, 44);
+            txtCodigoProduto.TabIndex = 10;
+            txtCodigoProduto.Text = "    CÓDIGO DO PRODUTO";
             // 
-            // textBox1
+            // txtCodigoCliente
             // 
-            textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Font = new Font("Bebas Neue", 17F);
-            textBox1.ForeColor = Color.FromArgb(147, 147, 147);
-            textBox1.Location = new Point(808, 156);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(274, 44);
-            textBox1.TabIndex = 9;
-            textBox1.Text = "    CÓDIGO DO CLIENTE";
+            txtCodigoCliente.BorderStyle = BorderStyle.FixedSingle;
+            txtCodigoCliente.Font = new Font("Bebas Neue", 17F);
+            txtCodigoCliente.ForeColor = Color.FromArgb(147, 147, 147);
+            txtCodigoCliente.Location = new Point(808, 156);
+            txtCodigoCliente.Name = "txtCodigoCliente";
+            txtCodigoCliente.Size = new Size(274, 44);
+            txtCodigoCliente.TabIndex = 9;
+            txtCodigoCliente.Text = "    CÓDIGO DO CLIENTE";
             // 
             // cmbData
             // 
@@ -361,17 +338,37 @@
             cmbCliente.TabIndex = 6;
             cmbCliente.Text = "    CLIENTE";
             // 
-            // frmCadastro2Encomendas
+            // lblTitulo2
+            // 
+            lblTitulo2.AutoSize = true;
+            lblTitulo2.Font = new Font("Bebas Neue", 30F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblTitulo2.ForeColor = Color.FromArgb(121, 121, 121);
+            lblTitulo2.Location = new Point(27, 19);
+            lblTitulo2.Name = "lblTitulo2";
+            lblTitulo2.Size = new Size(186, 38);
+            lblTitulo2.TabIndex = 3;
+            lblTitulo2.Text = "BAIXA NO ESTOQUE";
+            // 
+            // lblTitulo1
+            // 
+            lblTitulo1.AutoSize = true;
+            lblTitulo1.Font = new Font("Bebas Neue", 33F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblTitulo1.ForeColor = Color.White;
+            lblTitulo1.Location = new Point(35, 25);
+            lblTitulo1.Name = "lblTitulo1";
+            lblTitulo1.Size = new Size(362, 43);
+            lblTitulo1.TabIndex = 1;
+            lblTitulo1.Text = "CADASTRO DE VENDA - ENCOMENDA";
+            // 
+            // FrmCadastro2Vendas
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
-            AutoSize = true;
             ClientSize = new Size(1481, 1055);
             Controls.Add(pnlFundo);
-            MinimumSize = new Size(0, 1018);
-            Name = "frmCadastro2Encomendas";
-            Text = "frmCadastro2Encomendas";
+            Name = "FrmCadastro2Vendas";
+            Text = "FrmCadastro2Vendas";
             pnlFundo.ResumeLayout(false);
             pnlFundo.PerformLayout();
             panel1.ResumeLayout(false);
@@ -381,20 +378,12 @@
 
         #endregion
 
-        private Label lblTitulo1;
         private Panel pnlFundo;
         private Panel panel1;
-        private TextBox txtQtdeParcelas;
-        private TextBox txtEntrada;
-        private TextBox txtValorTotal;
-        private TextBox txtQuantidade;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private ComboBox cmbData;
-        private ComboBox cmbProduto;
-        private ComboBox cmbCliente;
+        private Button btnCancelar;
+        private Button btnConfirmar;
         private TextBox txtQtdeParcelas2;
-        private TextBox txtEntrada2;
+        private TextBox txtFormaPgto2;
         private TextBox txtValorTotal2;
         private TextBox txtQtde2;
         private TextBox txtCodigoProduto2;
@@ -402,7 +391,15 @@
         private ComboBox cmbData2;
         private ComboBox cmbProduto2;
         private ComboBox cmbCliente2;
-        private Button btnCancelar;
-        private Button btnConfirmar;
+        private TextBox txtQtdeParcelas;
+        private TextBox txtFormaPgto;
+        private TextBox txtValorTotal;
+        private TextBox txtQuantidade;
+        private TextBox txtCodigoProduto;
+        private TextBox txtCodigoCliente;
+        private ComboBox cmbData;
+        private ComboBox cmbProduto;
+        private ComboBox cmbCliente;
+        private Label lblTitulo1;
     }
 }
