90cc276a6bce128822d6dc1cca3409f56917a8ef
mariaclararicha123@gmail.com <mariaclararicha123@gmail.com>
2025-10-08 23:59:53 -0300

    Criação da tela de Pedidos em falta 2 Poup

---
+++ Atron/FrmPedidosEmFalta2.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Atron
+{
+    partial class FrmPedidosEmFalta2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPedidosEmFalta2));
+            lbl_QtdeRPop = new Label();
+            lbl_CodProdutoRPop = new Label();
+            pic_ProdutoPop = new PictureBox();
+            lbl_CodProdutoTPop = new Label();
+            lbl_QtdeTpop = new Label();
+            lbl_MarcaPop = new Label();
+            panel1 = new Panel();
+            lbl_PopUpNomeProduto = new Label();
+            ptbAlerta = new PictureBox();
+            lblInfNome = new Label();
+            lblNomeProduto = new Label();
+            lblInfQuantMin = new Label();
+            lblQuantMin = new Label();
+            lbl_FornecedorRPop = new Label();
+            lbl_FornecedorPop = new Label();
+            lblData = new Label();
+            lblReposisao = new Label();
+            btnOk = new Button();
+            ((System.ComponentModel.ISupportInitialize)pic_ProdutoPop).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbAlerta).BeginInit();
+            SuspendLayout();
+            // 
+            // lbl_QtdeRPop
+            // 
+            lbl_QtdeRPop.AutoSize = true;
+            lbl_QtdeRPop.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_QtdeRPop.ForeColor = Color.FromArgb(147, 147, 147);
+            lbl_QtdeRPop.Location = new Point(319, 300);
+            lbl_QtdeRPop.Name = "lbl_QtdeRPop";
+            lbl_QtdeRPop.Size = new Size(31, 33);
+            lbl_QtdeRPop.TabIndex = 43;
+            lbl_QtdeRPop.Text = "3";
+            // 
+            // lbl_CodProdutoRPop
+            // 
+            lbl_CodProdutoRPop.AutoSize = true;
+            lbl_CodProdutoRPop.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_CodProdutoRPop.ForeColor = Color.FromArgb(147, 147, 147);
+            lbl_CodProdutoRPop.Location = new Point(319, 224);
+            lbl_CodProdutoRPop.Name = "lbl_CodProdutoRPop";
+            lbl_CodProdutoRPop.Size = new Size(31, 33);
+            lbl_CodProdutoRPop.TabIndex = 40;
+            lbl_CodProdutoRPop.Text = "1";
+            // 
+            // pic_ProdutoPop
+            // 
+            pic_ProdutoPop.Image = (Image)resources.GetObject("pic_ProdutoPop.Image");
+            pic_ProdutoPop.Location = new Point(542, 139);
+            pic_ProdutoPop.Name = "pic_ProdutoPop";
+            pic_ProdutoPop.Size = new Size(299, 260);
+            pic_ProdutoPop.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_ProdutoPop.TabIndex = 39;
+            pic_ProdutoPop.TabStop = false;
+            // 
+            // lbl_CodProdutoTPop
+            // 
+            lbl_CodProdutoTPop.AutoSize = true;
+            lbl_CodProdutoTPop.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lbl_CodProdutoTPop.ForeColor = Color.FromArgb(255, 122, 0);
+            lbl_CodProdutoTPop.Location = new Point(12, 219);
+            lbl_CodProdutoTPop.Name = "lbl_CodProdutoTPop";
+            lbl_CodProdutoTPop.Size = new Size(301, 39);
+            lbl_CodProdutoTPop.TabIndex = 38;
+            lbl_CodProdutoTPop.Text = "Código do Poduto:";
+            // 
+            // lbl_QtdeTpop
+            // 
+            lbl_QtdeTpop.AutoSize = true;
+            lbl_QtdeTpop.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lbl_QtdeTpop.ForeColor = Color.FromArgb(255, 122, 0);
+            lbl_QtdeTpop.Location = new Point(12, 295);
+            lbl_QtdeTpop.Name = "lbl_QtdeTpop";
+            lbl_QtdeTpop.Size = new Size(293, 39);
+            lbl_QtdeTpop.TabIndex = 36;
+            lbl_QtdeTpop.Text = "Quantidade Atual:";
+            // 
+            // lbl_MarcaPop
+            // 
+            lbl_MarcaPop.AutoSize = true;
+            lbl_MarcaPop.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lbl_MarcaPop.ForeColor = Color.FromArgb(255, 122, 0);
+            lbl_MarcaPop.Location = new Point(12, 315);
+            lbl_MarcaPop.Name = "lbl_MarcaPop";
+            lbl_MarcaPop.Size = new Size(0, 39);
+            lbl_MarcaPop.TabIndex = 35;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(119, 162, 255);
+            panel1.Controls.Add(ptbAlerta);
+            panel1.Controls.Add(lbl_PopUpNomeProduto);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(868, 97);
+            panel1.TabIndex = 34;
+            // 
+            // lbl_PopUpNomeProduto
+            // 
+            lbl_PopUpNomeProduto.AutoSize = true;
+            lbl_PopUpNomeProduto.Font = new Font("Microsoft Sans Serif", 33.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_PopUpNomeProduto.ForeColor = Color.White;
+            lbl_PopUpNomeProduto.Location = new Point(350, 24);
+            lbl_PopUpNomeProduto.Name = "lbl_PopUpNomeProduto";
+            lbl_PopUpNomeProduto.Size = new Size(180, 52);
+            lbl_PopUpNomeProduto.TabIndex = 0;
+            lbl_PopUpNomeProduto.Text = "Produto";
+            // 
+            // ptbAlerta
+            // 
+            ptbAlerta.Image = (Image)resources.GetObject("ptbAlerta.Image");
+            ptbAlerta.Location = new Point(292, 24);
+            ptbAlerta.Name = "ptbAlerta";
+            ptbAlerta.Size = new Size(47, 49);
+            ptbAlerta.TabIndex = 1;
+            ptbAlerta.TabStop = false;
+            // 
+            // lblInfNome
+            // 
+            lblInfNome.AutoSize = true;
+            lblInfNome.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblInfNome.ForeColor = Color.FromArgb(147, 147, 147);
+            lblInfNome.Location = new Point(322, 145);
+            lblInfNome.Name = "lblInfNome";
+            lblInfNome.Size = new Size(213, 33);
+            lblInfNome.TabIndex = 45;
+            lblInfNome.Text = "Meia Cano Alto";
+            // 
+            // lblNomeProduto
+            // 
+            lblNomeProduto.AutoSize = true;
+            lblNomeProduto.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lblNomeProduto.ForeColor = Color.FromArgb(255, 122, 0);
+            lblNomeProduto.Location = new Point(12, 139);
+            lblNomeProduto.Name = "lblNomeProduto";
+            lblNomeProduto.Size = new Size(297, 39);
+            lblNomeProduto.TabIndex = 44;
+            lblNomeProduto.Text = "Nome do Produto:";
+            // 
+            // lblInfQuantMin
+            // 
+            lblInfQuantMin.AutoSize = true;
+            lblInfQuantMin.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblInfQuantMin.ForeColor = Color.FromArgb(147, 147, 147);
+            lblInfQuantMin.Location = new Point(345, 381);
+            lblInfQuantMin.Name = "lblInfQuantMin";
+            lblInfQuantMin.Size = new Size(47, 33);
+            lblInfQuantMin.TabIndex = 47;
+            lblInfQuantMin.Text = "40";
+            // 
+            // lblQuantMin
+            // 
+            lblQuantMin.AutoSize = true;
+            lblQuantMin.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lblQuantMin.ForeColor = Color.FromArgb(255, 122, 0);
+            lblQuantMin.Location = new Point(12, 375);
+            lblQuantMin.Name = "lblQuantMin";
+            lblQuantMin.Size = new Size(327, 39);
+            lblQuantMin.TabIndex = 46;
+            lblQuantMin.Text = "Quantidade Mínima:";
+            // 
+            // lbl_FornecedorRPop
+            // 
+            lbl_FornecedorRPop.AutoSize = true;
+            lbl_FornecedorRPop.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_FornecedorRPop.ForeColor = Color.FromArgb(147, 147, 147);
+            lbl_FornecedorRPop.Location = new Point(226, 470);
+            lbl_FornecedorRPop.Name = "lbl_FornecedorRPop";
+            lbl_FornecedorRPop.Size = new Size(164, 33);
+            lbl_FornecedorRPop.TabIndex = 49;
+            lbl_FornecedorRPop.Text = "Fornecedor";
+            // 
+            // lbl_FornecedorPop
+            // 
+            lbl_FornecedorPop.AutoSize = true;
+            lbl_FornecedorPop.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lbl_FornecedorPop.ForeColor = Color.FromArgb(255, 122, 0);
+            lbl_FornecedorPop.Location = new Point(16, 464);
+            lbl_FornecedorPop.Name = "lbl_FornecedorPop";
+            lbl_FornecedorPop.Size = new Size(204, 39);
+            lbl_FornecedorPop.TabIndex = 48;
+            lbl_FornecedorPop.Text = "Fornecedor:";
+            // 
+            // lblData
+            // 
+            lblData.AutoSize = true;
+            lblData.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblData.ForeColor = Color.FromArgb(147, 147, 147);
+            lblData.Location = new Point(322, 556);
+            lblData.Name = "lblData";
+            lblData.Size = new Size(159, 33);
+            lblData.TabIndex = 51;
+            lblData.Text = "31/02/2025";
+            // 
+            // lblReposisao
+            // 
+            lblReposisao.AutoSize = true;
+            lblReposisao.Font = new Font("Microsoft Sans Serif", 26.25F, FontStyle.Underline);
+            lblReposisao.ForeColor = Color.FromArgb(255, 122, 0);
+            lblReposisao.Location = new Point(13, 550);
+            lblReposisao.Name = "lblReposisao";
+            lblReposisao.Size = new Size(300, 39);
+            lblReposisao.TabIndex = 50;
+            lblReposisao.Text = "Última Reposição:";
+            // 
+            // btnOk
+            // 
+            btnOk.Image = (Image)resources.GetObject("btnOk.Image");
+            btnOk.Location = new Point(762, 598);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(106, 108);
+            btnOk.TabIndex = 52;
+            btnOk.UseVisualStyleBackColor = true;
+            // 
+            // FrmPedidosEmFalta2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(868, 706);
+            Controls.Add(btnOk);
+            Controls.Add(lblData);
+            Controls.Add(lblReposisao);
+            Controls.Add(lbl_FornecedorRPop);
+            Controls.Add(lbl_FornecedorPop);
+            Controls.Add(lblInfQuantMin);
+            Controls.Add(lblQuantMin);
+            Controls.Add(lblInfNome);
+            Controls.Add(lblNomeProduto);
+            Controls.Add(lbl_QtdeRPop);
+            Controls.Add(lbl_CodProdutoRPop);
+            Controls.Add(pic_ProdutoPop);
+            Controls.Add(lbl_CodProdutoTPop);
+            Controls.Add(lbl_QtdeTpop);
+            Controls.Add(lbl_MarcaPop);
+            Controls.Add(panel1);
+            Name = "FrmPedidosEmFalta2";
+            Text = "FrmPedidosEmFalta2";
+            ((System.ComponentModel.ISupportInitialize)pic_ProdutoPop).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbAlerta).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_QtdeRPop;
+        private Label lbl_CodProdutoRPop;
+        private PictureBox pic_ProdutoPop;
+        private Label lbl_CodProdutoTPop;
+        private Label lbl_QtdeTpop;
+        private Label lbl_MarcaPop;
+        private Panel panel1;
+        private PictureBox ptbAlerta;
+        private Label lbl_PopUpNomeProduto;
+        private Label lblInfNome;
+        private Label lblNomeProduto;
+        private Label lblInfQuantMin;
+        private Label lblQuantMin;
+        private Label lbl_FornecedorRPop;
+        private Label lbl_FornecedorPop;
+        private Label lblData;
+        private Label lblReposisao;
+        private Button btnOk;
+    }
+}
---
+++ Atron/FrmPedidosEmFalta2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atron
+{
+    public partial class FrmPedidosEmFalta2 : Form
+    {
+        public FrmPedidosEmFalta2()
+        {
+            InitializeComponent();
+        }
+    }
+}
