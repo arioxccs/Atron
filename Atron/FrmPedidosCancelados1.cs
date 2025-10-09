2d4c39d1a27a613093baa89a1e44048e6f639421
Corimbaba <joaovictorcorimbaba@gmail.com>
2025-10-09 10:05:54 -0300

    Criacao da PedidosCancelados1

---
+++ Atron/FrmPedidosCancelados1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atron
+{
+    public partial class FrmPedidosCancelados1 : Form
+    {
+        public FrmPedidosCancelados1()
+        {
+            InitializeComponent();
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblValor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
